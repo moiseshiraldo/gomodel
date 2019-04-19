e7fd15bc61d7f841f3e46bc1f39f0af3117458e3
Moises Hiraldo <moiseshiraldo@gmail.com>
2019-04-19 22:05:53 +0100

    Define add/remove fields operations

--- migrations/operations.go
+++ migrations/operations.go
@@ -110,12 +110,77 @@ func (op DeleteModel) FromJSON(raw []byte) (Operation, error) {
 }
 
 func (op DeleteModel) SetState(state *AppState) error {
+	if _, ok := state.Models[op.Model]; !ok {
+		return fmt.Errorf("delete model not found: %s", op.Model)
+	}
 	delete(state.Models, op.Model)
+	return nil
+}
+
+type AddFields struct {
+	Model  string
+	Fields gomodels.Fields
+}
+
+func (op AddFields) Name() string {
+	return "AddFields"
+}
+
+func (op AddFields) FromJSON(raw []byte) (Operation, error) {
+	err := json.Unmarshal(raw, &op)
+	return op, err
+}
+
+func (op AddFields) SetState(state *AppState) error {
+	if _, ok := state.Models[op.Model]; !ok {
+		return fmt.Errorf("add fields: model not found: %s", op.Model)
+	}
+	fields := state.Models[op.Model].Fields()
+	for name, field := range op.Fields {
+		if _, found := fields[name]; found {
+			return fmt.Errorf("%s: duplicate field: %s", op.Model, name)
+		}
+		fields[name] = field
+	}
+	delete(state.Models, op.Model)
+	state.Models[op.Model] = gomodels.New(op.Model, fields)
+	return nil
+}
+
+type RemoveFields struct {
+	Model  string
+	Fields []string
+}
+
+func (op RemoveFields) Name() string {
+	return "RemoveFields"
+}
+
+func (op RemoveFields) FromJSON(raw []byte) (Operation, error) {
+	err := json.Unmarshal(raw, &op)
+	return op, err
+}
+
+func (op RemoveFields) SetState(state *AppState) error {
+	if _, ok := state.Models[op.Model]; !ok {
+		return fmt.Errorf("remove fields: model not found: %s", op.Model)
+	}
+	fields := state.Models[op.Model].Fields()
+	for _, name := range op.Fields {
+		if _, ok := fields[name]; !ok {
+			return fmt.Errorf("%s: remove field not found: %s", op.Model, name)
+		}
+		delete(fields, name)
+	}
+	delete(state.Models, op.Model)
+	state.Models[op.Model] = gomodels.New(op.Model, fields)
 	return nil
 }
 
 func AvailableOperations() map[string]Operation {
 	return map[string]Operation{
 		"CreateModel": CreateModel{},
+		"DeleteModel": DeleteModel{},
+		"AddFields":   AddFields{},
 	}
 }
