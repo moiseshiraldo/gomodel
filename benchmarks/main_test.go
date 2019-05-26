1091853e5c1bdd8738cdcae2e6a4d6344c4c63c3
Moises Hiraldo <moiseshiraldo@gmail.com>
2019-05-26 20:39:49 +0100

    Add benchmarks

---
+++ benchmarks/main_test.go
@@ -0,0 +1,25 @@
+package benchmarks
+
+import (
+    "testing"
+    "github.com/moiseshiraldo/gomodels"
+    "github.com/moiseshiraldo/gomodels/migrations"
+    "os"
+    "fmt"
+    _ "github.com/gwenn/gosqlite"
+)
+
+func TestMain(m *testing.M) {
+    app := gomodels.NewApp("main", "", User.Model)
+    gomodels.Register(app)
+    gomodels.Start(map[string]gomodels.Database{
+        "default": gomodels.Database{Driver: "sqlite3", Name: ":memory:"},
+    })
+    defer gomodels.Stop()
+    err := migrations.MakeAndRun()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "%s", err)
+        os.Exit(1)
+    }
+    os.Exit(m.Run())
+}
