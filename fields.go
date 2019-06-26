975c368a5cc126479cb12654498b51378663fa3b
Moises Hiraldo <moiseshiraldo@gmail.com>
2019-06-26 19:30:20 +0100

    Remove unused functions

--- fields.go
+++ fields.go
@@ -70,3 +70,18 @@ func fieldInList(name string, fields []string) bool {
 	}
 	return false
 }
+
+func sqlColumnOptions(null bool, pk bool, unique bool) string {
+	options := ""
+	if null {
+		options += " NULL"
+	} else {
+		options += " NOT NULL"
+	}
+	if pk {
+		options += " PRIMARY KEY"
+	} else if unique {
+		options += " UNIQUE"
+	}
+	return options
+}
